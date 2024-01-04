cb32461f5512e096e026ea9225254263a428f295
Riley Hoyson <riley.hoyson@icloud.com>
2024-01-04 05:18:06 -0500

    Declare getComputerChoice function
    Declared function named getComputerChoice which generates the computer's
    choice between rock, paper, and scissors. This function generates a
    random number between 0-2 and assigns "rock", "paper", and "scissors"
    to to each possible outcome respectively using a conditional statement.

---
+++ script.js
@@ -0,0 +1,8 @@
+let getComputerChoice = () => {
+    let randomNumber = Math.floor(Math.random() * 3)
+    if (randomNumber === 0) {
+        return "rock";
+    } else if (randomNumber === 1) {
+        return "paper";
+    } return "scissors"
+}
