47330e39a731c68225647fb6b27158a3ed87947a
Riley Hoyson <riley.hoyson@icloud.com>
2024-01-04 05:41:38 -0500

    Declare playRound function
    Declared function playRound. This function takes two arguments:
    "playerSelection" and "computerSelection". It first evaluates the
    "playerSelection" argument using an if .. else conditional statement and
    compares this argument with the "computerSelection" argument using a
    switch statement to determine the winner of the round.

--- script.js
+++ script.js
@@ -6,3 +6,44 @@ let getComputerChoice = () => {
         return "paper";
     } return "scissors"
 }
+
+let playRound = (playerSelection, computerSelection) => {
+    if (playerSelection == "rock") {
+        switch (computerSelection) {
+            case "rock":
+                return "Tie.";
+                break;
+            case "paper":
+                return "Paper beats rock. You lose.";
+                break;
+            case "scissors":
+                return "Rock beats scissors. You win.";
+        }
+    }
+    else if (playerSelection == "paper") {
+        switch (computerSelection) {
+            case "rock":
+                return "Paper beats rock. You win.";
+                break;
+            case "paper":
+                return "Tie.";
+                break;
+            case "scissors":
+                return "Scissor beats paper. You lose.";
+        }   
+    } 
+    switch (computerSelection) {
+        case "rock":
+            return "Rock beats scissors. You lose.";
+            break;
+        case "paper":
+            return "Scissors beats paper. You win.";
+            break;
+        case "scissors":
+            return "Tie.";
+    } 
+}  
+
+const playerChoice = "rock";
+
+console.log(playRound(playerChoice.toLowerCase(), getComputerChoice()));
