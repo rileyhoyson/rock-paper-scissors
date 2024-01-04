9f242a0c420a9c147326ce1214d264ae3637b7ab
Riley Hoyson <riley.hoyson@icloud.com>
2024-01-04 08:26:29 -0500

    Print individual round results
    Declared function printRoundResult to log individual round results to
    the console. This function takes the player choice and round result as
    arguments and uses an if .. else conditional statement to evaluate
    the result. If the result is a win or loss, it then accounts for the
    player choice using a switch statement to determine what exact message
    to print i.e. rock beats paper. If the result is a tie the function
    returns and nothing is printed.
    
    printRoundResult is then invoked in every loop of the game function to
    print the result of each round.

--- script.js
+++ script.js
@@ -1,62 +1,93 @@
 let getComputerChoice = () => {
-    let randomNumber = Math.floor(Math.random() * 3)
-    if (randomNumber === 0) {
-        return "rock";
-    } else if (randomNumber === 1) {
-        return "paper";
-    } return "scissors"
-}
+  let randomNumber = Math.floor(Math.random() * 3);
+  if (randomNumber === 0) {
+    return "rock";
+  } else if (randomNumber === 1) {
+    return "paper";
+  }
+  return "scissors";
+};
 
 let playRound = (playerSelection, computerSelection) => {
-    if (playerSelection == "rock") {
-        switch (computerSelection) {
-            case "rock":
-                return "Tie";
-                break;
-            case "paper":
-                return "Lose";
-                break;
-            case "scissors":
-                return "Win";
-        }
-    }
-    else if (playerSelection == "paper") {
-        switch (computerSelection) {
-            case "rock":
-                return "Win";
-                break;
-            case "paper":
-                return "Tie";
-                break;
-            case "scissors":
-                return "Lose";
-        }   
-    } 
+  if (playerSelection == "rock") {
     switch (computerSelection) {
+      case "rock":
+        return "Tie";
+        break;
+      case "paper":
+        return "Lose";
+        break;
+      case "scissors":
+        return "Win";
+    }
+  } else if (playerSelection == "paper") {
+    switch (computerSelection) {
+      case "rock":
+        return "Win";
+        break;
+      case "paper":
+        return "Tie";
+        break;
+      case "scissors":
+        return "Lose";
+    }
+  }
+  switch (computerSelection) {
+    case "rock":
+      return "Win";
+      break;
+    case "paper":
+      return "Lose";
+      break;
+    case "scissors":
+      return "Tie";
+  }
+};
+
+const printRoundResult = (playerSelection, playRoundResult) => {
+  let result;
+  if (playRoundResult == "Win") {
+    switch (playerSelection) {
+      case "rock":
+        result = "Rock beats scissors. You win.";
+        break;
+      case "paper":
+        result = "Paper beats rock. You win.";
+        break;
+      case "scissors":
+        result = "Scissors beats paper. You win.";
+    }
+  } else if (playRoundResult == "Lose") {
+    switch (playerSelection) {
         case "rock":
-            return "Win";
-            break;
+          result = "Paper beats rock. You lose.";
+          break;
         case "paper":
-            return "Lose";
-            break;
+          result = "Scissor beats paper. You lose.";
+          break;
         case "scissors":
-            return "Tie";
-    } 
-}  
+          result = "Rock beats scissors. You lose.";
+  }
+  } else {
+    return;
+  }
+  console.log(result);
+};
 
 const playerChoice = "rock";
 
 let game = () => {
-    let score = 0;
-    for (let i = 0; i < 5; i++) {
-        let roundResult = playRound(playerChoice.toLowerCase(), getComputerChoice())
-        if (roundResult == "Win") {
-            score++;
-        } else if (roundResult == "Tie") {
-            i--;
-        }
+  let score = 0;
+  for (let i = 0; i < 5; i++) {
+    let roundResult = playRound(playerChoice.toLowerCase(), getComputerChoice());
+    printRoundResult(playerChoice, roundResult);
+    if (roundResult == "Win") {
+      score++;
+    } else if (roundResult == "Tie") {
+      i--;
     }
-    console.log(`You won ${score} of 5 rounds.`)
-}
+  }
+  console.log(`You won ${score} of 5 rounds.`);
+};
 
 game();
